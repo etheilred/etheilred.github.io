d6398fc4ef6fda2f536301363c369f02545d159a
roman <rdandrievskiy@edu.hse.ru>
2020-06-19 00:35:28 +0300

    Updates

---
+++ precache-manifest.7bab938e8426c9c57bc78b79d912c547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e5d078f8ac70f8422fa43797c911bf",
+    "url": "/currency_translator/index.html"
+  },
+  {
+    "revision": "e56c5b293da589f024ea",
+    "url": "/currency_translator/static/css/main.dc9852b1.chunk.css"
+  },
+  {
+    "revision": "6e341d98d7a65b6f24a5",
+    "url": "/currency_translator/static/js/2.955c9566.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency_translator/static/js/2.955c9566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56c5b293da589f024ea",
+    "url": "/currency_translator/static/js/main.d3a94714.chunk.js"
+  },
+  {
+    "revision": "d78fbb997c1fdd0fdf23",
+    "url": "/currency_translator/static/js/runtime-main.6808ee6f.js"
+  }
+]);
